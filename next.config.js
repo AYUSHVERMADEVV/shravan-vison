8f2c82884de0ec25afdd823b76898705b072eab0
AYUSHVERMADEVV <av9918044987@gmail.com>
2025-09-08 10:47:15 +0530

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
     config.ignoreWarnings = [
       { module: /node_modules\/node-fetch\/lib\/index\.js/ },
       { file: /node_modules\/node-fetch\/lib\/index\.js/ },
-    }
+    ];
     
     return config;
   },
